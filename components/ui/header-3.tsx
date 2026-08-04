b65323583c25b8ca27746e826d97799e233498f6
Adarsh <adarsh@huntme.in>
2026-08-04 19:43:22 +0530

    chore: Re-deploy header navigation update to clear Vercel cache

--- components/ui/header-3.tsx
+++ components/ui/header-3.tsx
@@ -1,5 +1,7 @@
 'use client';
+// Updated navigation header without Blog
 import React from 'react';
+
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { MenuToggleIcon } from '@/components/ui/menu-toggle-icon';
